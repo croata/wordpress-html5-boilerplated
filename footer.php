1d3cfa6c06c5bf9b1dbadecac298f90007178321
Dayle Rees <thepunkfan@gmail.com>
2011-06-24 10:26:28 +0100

    First commit.

---
+++ footer.php
@@ -0,0 +1,26 @@
+
+                </div>
+                <footer>
+
+                </footer>
+        </div> <!--! end of #container -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="<?php echo bloginfo('template_url'); ?>/js/libs/jquery-1.6.1.min.js"><\/script>')</script>
+        <script src="<?php echo bloginfo('template_url'); ?>/js/plugins.js"></script>
+        <script src="<?php echo bloginfo('template_url'); ?>/js/script.js"></script>
+
+        <script>
+        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview'],['_trackPageLoadTime']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+
+        <!--[if lt IE 7 ]>
+        <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
+        <script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
+        <![endif]-->
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+        <title></title>
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="shortcut icon" href="<?php echo bloginfo('template_url'); ?>/favicon.ico"> 
+        <link rel="apple-touch-icon" href="<?php echo bloginfo('template_url'); ?>/apple-touch-icon.png"> 
+        <link rel="stylesheet" href="<?php echo bloginfo('template_url'); ?>/css/style.css">
+
+        <script src="<?php echo bloginfo('template_url'); ?>/js/libs/modernizr-2.0.min.js"></script>
+        <script src="<?php echo bloginfo('template_url'); ?>/js/libs/respond.min.js"></script>
+</head>
+
+<body>
+
+        <div id="container">
+                <header>
+
+                </header>
+                <div id="main" role="main">
